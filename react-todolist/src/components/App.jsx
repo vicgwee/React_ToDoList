1ba9e255b55c1e2b4365d0d6dd3db7fc3395bcee
Victor Gwee <47884004+vicgwee@users.noreply.github.com>
2021-12-20 14:11:32 +0800

    use map() to generate new Notes

--- react-todolist/src/components/App.jsx
+++ react-todolist/src/components/App.jsx
@@ -4,13 +4,18 @@ import Footer from "./Footer.jsx";
 import Note from "./Note.jsx";
 import notes from "../notes"
 
+
+function createNote(note){
+    return <Note 
+    key={note.id}
+    header={note.header}
+    content={note.content}/>;
+}
+
 function App() {
     return <div>
         <Header />
-        <Note header={notes[0].header}
-         content={notes[0].content}/>
-        <Note header={notes[1].header}
-         content={notes[1].content}/>
+        {notes.map(createNote)}
         <Footer />
     </div>;
 }
--- react-todolist/src/notes.js
+++ react-todolist/src/notes.js
@@ -1,9 +1,11 @@
 const notes = [
     {
+        id:"1",
         header: "Make a website",
         content: "Does this count?"
     },
     {
+        id:"2",
         header: "Make more websites",
         content: "just do it"
     }
