cfecd70d7f1d0a4dbbe86667ae117b93dbbfcc94
Victor Gwee <47884004+vicgwee@users.noreply.github.com>
2021-12-20 20:28:49 +0800

    use hooks to get inputs from NewNote

--- react-todolist/src/components/App.jsx
+++ react-todolist/src/components/App.jsx
@@ -3,6 +3,7 @@ import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Note from "./Note.jsx";
 import notes from "../notes"
+import NewNote from "./NewNote.jsx";
 
 function createNote(note) {
     return <Note 
@@ -14,6 +15,7 @@ function createNote(note) {
 function App() {
     return <div>
         <Header />
+        <NewNote />
         {notes.map(createNote)}
         <Footer />
     </div>;
--- react-todolist/src/components/NewNote.jsx
+++ react-todolist/src/components/NewNote.jsx
@@ -1,11 +1,46 @@
-import React from "react"
-import Note from "./Note";
+import React, {useState} from "react"
+import notes from "../notes";
 
-function NewNote(props) {
-    return <Note
-        title=<input type="text" placeholder="Enter your title"/>
-        content=<input type="text" placeholder="Enter your content"/>
-    />;
-}
+// From https://www.w3schools.com/react/react_forms.asp
+
+function NewNote() {
+    const [inputs, setInputs] = useState({});
+    const handleChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setInputs(values => ({...values, [name]: value}));
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let nextId = notes.length + 1
+        notes.push( {
+            id:nextId.toString(),
+            title:inputs.title,
+            content: inputs.content
+        });
+        nextId += 1
+        console.log(notes);
+    }
+  
+    return <div className="note">
+        <h1>New Note</h1>
+        <form onSubmit={handleSubmit}>
+            <input 
+                type="text"
+                name="title"
+                value = {inputs.title || ""}
+                onChange={handleChange}
+            />
+            <input 
+                type="text"
+                name="content"
+                value = {inputs.content || ""}
+                onChange={handleChange}
+            />
+            <button type="submit">Add</button>
+        </form>
+    </div>;
+  }
 
 export default NewNote;
