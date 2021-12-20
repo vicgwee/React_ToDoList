642550ca65980175d7a8de977e75e86aee1e275d
Victor Gwee <47884004+vicgwee@users.noreply.github.com>
2021-12-20 13:57:49 +0800

    separate out notes content

--- react-todolist/src/components/App.jsx
+++ react-todolist/src/components/App.jsx
@@ -1,12 +1,16 @@
-import React from "react"
-import Header from "./Header.jsx"
-import Footer from "./Footer.jsx"
+import React from "react";
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
 import Note from "./Note.jsx";
+import notes from "../notes"
 
 function App() {
     return <div>
         <Header />
-        <Note header="This is a note" content="Note about something"/>
+        <Note header={notes[0].header}
+         content={notes[0].content}/>
+        <Note header={notes[1].header}
+         content={notes[1].content}/>
         <Footer />
     </div>;
 }
---
+++ react-todolist/src/notes.js
@@ -0,0 +1,12 @@
+const notes = [
+    {
+        header: "Make a website",
+        content: "Does this count?"
+    },
+    {
+        header: "Make more websites",
+        content: "just do it"
+    }
+]
+
+export default notes;
