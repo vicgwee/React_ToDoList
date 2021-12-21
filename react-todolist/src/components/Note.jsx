a51cc820984138ec05ff4dcbe133c46d3878d32e
Victor Gwee <47884004+vicgwee@users.noreply.github.com>
2021-12-21 08:45:16 +0800

    add done state and toggle it on click

--- react-todolist/src/components/Note.jsx
+++ react-todolist/src/components/Note.jsx
@@ -1,14 +1,21 @@
-import React from "react"
+import React, { useState } from "react"
 
 function Note(props) {
-    function handleClick(){
+    const [done, setDone] = useState(0);
+
+    function handleNoteClick(){
+        setDone(val => 1-val);
+        console.log("Clicked note %s, done is %i", props.id, done);
+    }
+
+    function handleDeleteButtonClick(){
         console.log("Deleting note id %s",props.id)
         props.onDelete(props.id)
     }
 
     return (
-        <div className="note">
-            <button type="button" onClick={handleClick}>x</button>
+        <div className={"note " + done.toString()} onClick={handleNoteClick}>
+            <button type="button" onClick={handleDeleteButtonClick}>x</button>
             <h1>{props.title}</h1>
             <p>{props.content}</p>
         </div>
